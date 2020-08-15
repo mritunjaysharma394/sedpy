a55b5fea04dd641e41f9474ed8162296209b2c9c
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-08-15 22:29:08 +0530

    Mininmal workable sed alternative replacement support added

---
+++ sedpy/__main__.py
@@ -0,0 +1,16 @@
+import sys
+import sedpy
+from .sedpy import replace
+
+def main():
+    print('in main')
+    args = sys.argv[1:]
+    print('count of args :: {}'.format(len(args)))
+    for arg in args:
+        print('passed argument :: {}'.format(arg))
+
+    replace(sys.argv[1],sys.argv[2],sys.argv[3])
+
+
+if __name__ == '__main__':
+    main()
---
+++ sedpy/sedpy.py
@@ -0,0 +1,21 @@
+import re
+
+def replace(oldstring, newstring, infile, dryrun=False):
+    '''
+    Sed-like Replace
+    Usage: pycli <Old string>  <Replacement String> <Text File>
+    Example: pycli 'xyz' 'XYZ' '/path/to/file.txt'
+    '''
+    linelist = []
+    with open(infile) as f:
+        for item in f:
+            newitem = re.sub(oldstring, newstring, item)    #String replacement taking place.
+            linelist.append(newitem)
+    if dryrun == False:
+        with open(infile, "w") as f:
+            f.truncate()
+            for line in linelist: f.writelines(line)    #Dry Run False means it will not be displayed in STDOUT, File will change internally.
+    elif dryrun == True:
+        for line in linelist: print(line, end='')      #Dry Run Shows the changes on STDOUT.
+    else:
+        exit("Unknown option specified to 'dryrun' argument, Usage: dryrun=<True|False>.")
