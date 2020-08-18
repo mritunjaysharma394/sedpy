9d3fb185c1026cd559c884c84f1fb7cd9a855bed
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-08-18 19:23:23 +0530

    Fixed the coding style of the test python file

--- tests/test_sedpy.py
+++ tests/test_sedpy.py
@@ -2,14 +2,13 @@ import unittest
 from sedpy import sedpy
 
 
-
 class NamesTestCase(unittest.TestCase):
+    def test_sedpy(self):
+        sedpy.replace("4", "6", "test1.txt")
+        f = open("test1.txt", "r")
+        self.assertEqual(f.read(), "7 + 6 = 13")
+        f.close()
 
-   def test_sedpy(self):
-       sedpy.replace("4", "6", "test1.txt")
-       f = open("test1.txt", "r")
-       self.assertEqual(f.read(), "7 + 6 = 13")
-       f.close()
 
 if __name__ == '__main__':
     unittest.main()
